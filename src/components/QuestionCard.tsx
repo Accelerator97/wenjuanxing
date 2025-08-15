724c8722f0aa4bf5aa3a780308142ca0da862589
Ben <928691436@qq.com>
2025-08-15 14:26:46 +0800

    feat: 基本列表页

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,48 @@
+import React, { FC } from 'react';
+import styles from './QuestionCard.module.scss';
+
+type PropsType = {
+  id: string;
+  title: string;
+  isPublished: boolean;
+  createdAt: string;
+  isStart: boolean;
+  answerCount: number;
+  deleteQuestion?: (id: string) => void;
+  editQuestion: (id: string) => void;
+};
+
+const QuestionCard: FC<PropsType> = (props: PropsType) => {
+  const { id, title, isPublished, deleteQuestion, editQuestion, createdAt, answerCount } = props;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <div className={styles.left}>
+          <a href="#">{title}</a>
+        </div>
+        <div className={styles.right}>
+          {isPublished ? <span style={{ color: 'green' }}>已发布</span> : <span>未发布</span>}
+          &nbsp;
+          <span>答卷: {answerCount}</span>
+          &nbsp;
+          <span>{createdAt}</span>
+        </div>
+      </div>
+
+      <div className={styles['button-container']}>
+        <div className={styles.left}>
+          <button>编辑问卷</button>
+          <button>问卷统计</button>
+        </div>
+        <div className={styles.right}>
+          <button>收藏</button>
+          <button>复制</button>
+          <button>删除</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
---
+++ src/pages/List.tsx
@@ -0,0 +1,98 @@
+import React, { FC, useState } from 'react';
+import Styles from './List.module.scss';
+import QuestionCard from '../components/QuestionCard';
+import { produce } from 'immer';
+
+const rawList = [
+  {
+    id: '1',
+    title: 'wenuan',
+    isPublished: false,
+    isStart: false,
+    answerCount: 5,
+    createdAt: '3月15日',
+  },
+  {
+    id: '2',
+    title: 'wenuan',
+    isPublished: false,
+    isStart: false,
+    answerCount: 5,
+    createdAt: '3月15日',
+  },
+  {
+    id: '3',
+    title: 'wenuan',
+    isPublished: true,
+    isStart: false,
+    answerCount: 5,
+    createdAt: '3月15日',
+  },
+  {
+    id: '4',
+    title: 'wenuan',
+    isPublished: false,
+    isStart: false,
+    answerCount: 5,
+    createdAt: '3月15日',
+  },
+];
+
+const List: FC = () => {
+  const [questionList, setQuestionList] = useState(rawList);
+
+  function add() {
+    setQuestionList(
+      produce(draft => {
+        draft.push({
+          isStart: false,
+          answerCount: 5,
+          createdAt: '3月15日',
+          id: Math.random() + '',
+          title: 'wenuan',
+          isPublished: false,
+        });
+      })
+    );
+  }
+
+  function del(id: string) {
+    setQuestionList(
+      produce(draft => {
+        const index = questionList.findIndex(r => r.id === id);
+        draft.splice(index, 1);
+      })
+    );
+  }
+
+  function edit(id: string) {
+    setQuestionList(
+      produce(draft => {
+        const index = questionList.findIndex(r => r.id === id);
+        draft[index].isPublished = true;
+      })
+    );
+  }
+
+  return (
+    <>
+      <div className={Styles.header}>
+        <div className={Styles.left}>
+          <h3>我的问卷</h3>
+        </div>
+        <div className={Styles.right}>
+          <h3>搜索</h3>
+        </div>
+      </div>
+      <div className={Styles.content}>
+        {questionList.map(item => {
+          const { id } = item;
+          return <QuestionCard key={id} {...item} editQuestion={edit} />;
+        })}
+      </div>
+      <div className={Styles.footer}>footer</div>
+    </>
+  );
+};
+
+export default List;
