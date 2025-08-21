dee7e6c0d95bbb97119d9c3bf1bbb2535d57746b
Ben <928691436@qq.com>
2025-08-21 16:47:24 +0800

    feat: 属性组件

--- src/components/QuestionComponents/QuestionTitle/index.ts
+++ src/components/QuestionComponents/QuestionTitle/index.ts
@@ -1,6 +1,6 @@
 import Component from './Component';
 import { QuestionTitleDefaultProps } from './interface';
-
+import PropComponent from './PropComponent';
 export * from './interface';
 
 // Title 组件的配置
@@ -8,5 +8,6 @@ export default {
   title: '标题',
   type: 'questionTitle', // 要和后端统一好
   Component,
+  PropComponent,
   defaultProps: QuestionTitleDefaultProps,
 };
